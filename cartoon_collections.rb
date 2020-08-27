3f511b4ea42d0fe03e896c11ef14f9faa884c649
Trey Jennings <tjennings343@gmail.com>
2020-08-27 21:11:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array = []
   cheese_types.each do |cheese|
-    if array.include?(cheese)
-      "#{cheese}"
+    array.include?(cheese)
   end
 
 end
